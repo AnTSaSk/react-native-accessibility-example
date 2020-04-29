103a589810c962d283cc6c2db11e227be8958291
Alexis Besson <alexis.besson@arneogroup.com>
2020-04-29 14:22:06 +0200

    Feature: Update Package.json with latest choice of library (removing use of css-modules and use of emotion) & update config

--- metro.config.js
+++ metro.config.js
@@ -19,7 +19,7 @@ module.exports = (async () => {
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'css', 'scss', 'sass', 'svg'],
+      sourceExts: [...sourceExts, 'svg'],
     },
   };
 })();
